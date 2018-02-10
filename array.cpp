4cff23e9b0c2b429d2e4040d51c8a8fa4b0b5986
Sudipta Biswas <31337864+SudiptaBiswas1804@users.noreply.github.com>
2018-02-10 08:51:39 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -7,11 +7,11 @@ int arr[n];
 cout<<"enter the size of the array "<<endl;
 cin>>n;
 cout<<"enter the array elements"<<endl;
-for(i=0; i<=n; i++) {
+for(i=0; i<=n-1; i++) {
 cin>>arr[i];
 }
 cout<<"the array elements are "<<endl;
-for(i=0; i<=n; i++){
+for(i=0; i<=n-1; i++){
 cout<<arr[i]<<" ";
 }
 return 0;
