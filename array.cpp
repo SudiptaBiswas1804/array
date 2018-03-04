ade1e1af5b685015aa9e3790f8cc1f5ed4af8e64
Sudipta Biswas <31337864+SudiptaBiswas1804@users.noreply.github.com>
2018-03-04 19:38:30 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -7,7 +7,7 @@ int arr[n];
 cout<<"enter the size of the array "<<endl;
 cin>>n;
 cout<<"enter the array elements"<<endl;
-for(i=0; i<=n-1; i++) {                 //or i<n
+for(i=0; i<=n-1; i++) {                 //or i<n, i/position starts from 0
 cin>>arr[i];
 }
 cout<<"the array elements are "<<endl;
@@ -16,3 +16,5 @@ cout<<arr[i]<<" ";
 }
 return 0;
 }
+
+//i means we are accessing the position of the array while arr[i] means we are accessing the data in the array.
